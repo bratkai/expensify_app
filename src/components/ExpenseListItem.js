c2b4dd1b4c50978f38d92c26b124257ec5bafb54
Barna <barna.ratkai@gmail.com>
2020-06-25 15:21:02 +0200

    HU locale set

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,13 +3,37 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
 
+// load a locale
+numeral.register('locale', 'hu', {
+  delimiters: {
+      thousands: '.',
+      decimal: ','
+  },
+  abbreviations: {
+      thousand: 'k',
+      million: 'M',
+      billion: 'Mrd',
+      trillion: 'Tr'
+  },
+  ordinal : function (number) {
+      return number === 1 ? '.' : '-ik';
+  },
+  currency: {
+      symbol: 'Ft'
+  }
+});
+
+// switch between locales
+numeral.locale('hu');
+
+
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
     <p>
-      {numeral(amount /100).format('$0,0.00')} 
+      {numeral(amount /100).format('$ 0,0[.]00')} 
       - 
       {moment(createdAt).format('MMMM Do, YYYY' )}
       </p>
