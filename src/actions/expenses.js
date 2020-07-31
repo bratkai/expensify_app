00fb2ee27b1f8dc564e33a851390c7c9184c4e75
Barna <barna.ratkai@gmail.com>
2020-07-31 13:24:09 +0200

    Store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -8,43 +8,38 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     const {
       description = '',
-        note = '',
-        amount = 0,
-        createdAt = 0
+      note = '',
+      amount = 0,
+      createdAt = 0
     } = expenseData;
+    const expense = { description, note, amount, createdAt };
 
-    const expense = {
-      description,
-      note,
-      amount,
-      createdAt
-    };
-    return database.ref('expenses').push(expense).then((ref) => {
+    return database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
-      }))
-    })
-  }
-}
+      }));
+    });
+  };
+};
 
 // REMOVE_EXPENSE
-export const removeExpense = ({
-  id
-} = {}) => ({
+export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
 
-export const startRemoveExpense = ({ id } = {} ) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).remove().then(() => {
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
       dispatch(removeExpense({ id }));
     });
-  }
+  };
 };
 
 // EDIT_EXPENSE
@@ -55,30 +50,34 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpense = (id, updates) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).update(updates).then(() => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
       dispatch(editExpense(id, updates));
-    })
-  }
+    });
+  };
 };
 
+// SET_EXPENSES
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
   expenses
 });
 
 export const startSetExpenses = () => {
-  return (dispatch => {
-    return database.ref('expenses').once('value').then((snapshot) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
       const expenses = [];
+
       snapshot.forEach((childSnapshot) => {
         expenses.push({
           id: childSnapshot.key,
           ...childSnapshot.val()
-        })
+        });
       });
 
       dispatch(setExpenses(expenses));
-    })
-  })
+    });
+  };
 };
