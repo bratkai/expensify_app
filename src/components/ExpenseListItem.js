6dbfeb18c93839f6d0f1c65922b725dddf4d8dc0
Barna <barna.ratkai@gmail.com>
2020-08-13 12:22:03 +0200

    Add Expensify styles

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -19,7 +19,7 @@ numeral.register('locale', 'hu', {
       return number === 1 ? '.' : '-ik';
   },
   currency: {
-      symbol: 'Ft'
+      symbol: 'Ft '
   }
 });
 
@@ -28,16 +28,13 @@ numeral.locale('hu');
 
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-  <div>
-    <Link to={`/edit/${id}`}>
-      <h3>{description}</h3>
+   <Link className="list-item" to={`/edit/${id}`}>
+     <div>
+      <h3 className="list-item__title">{description}</h3>
+      <span className="list-item__sub-title">{moment(createdAt).format('MMMM Do, YYYY' )}</span>      
+     </div>
+     <h3 className="list-item__data">{numeral(amount /100).format('$ 0,0[.]00')} </h3>
     </Link>
-    <p>
-      {numeral(amount /100).format('$ 0,0[.]00')} 
-      - 
-      {moment(createdAt).format('MMMM Do, YYYY' )}
-      </p>
-  </div>
-);
+   );
 
 export default ExpenseListItem;
